178bd1ad2bad2afb01ea41195276f8f2e71c522e
KiritoNya <watashiwayuridaisuki@gmail.com>
2022-05-24 00:14:45 +0200

    Fix languages method and add tests

--- api/internal/models/languages.go
+++ api/internal/models/languages.go
@@ -1,20 +1,20 @@
 package models
 
 type Language struct {
-	Id           int    `json:"id"`
+	Id           int64  `json:"id"`
 	Abbreviation string `json:"abbreviation,omitempty"`
 	Name         string `json:"name"`
 }
 
-// NewLanguage is a function that adds a new language into database
-func NewLanguage(language Language) (int64, error) {
+// NewLanguage is a language method that adds a new language into database
+func (l *Language) NewLanguage() (int64, error) {
 
 	qp, err := DbConn.Prepare(`INSERT INTO Languages(abbreviation, name) VALUES (?, ?)`)
 	if err != nil {
 		return -1, err
 	}
 
-	res, err := qp.Exec(language.Abbreviation, language.Name)
+	res, err := qp.Exec(l.Abbreviation, l.Name)
 	if err != nil {
 		return -1, err
 	}
@@ -23,7 +23,7 @@ func NewLanguage(language Language) (int64, error) {
 }
 
 // GetLanguageById is a function that gets the language from the database by id
-func (l *Language) GetLanguageById(id int64) (*Language, error) {
+func GetLanguageById(id int64) (*Language, error) {
 
 	var lang Language
 
@@ -32,13 +32,18 @@ func (l *Language) GetLanguageById(id int64) (*Language, error) {
 		return nil, err
 	}
 
-	rows, err := qp.Query()
+	rows, err := qp.Query(id)
 	if err != nil {
 		return nil, err
 	}
 
 	for rows.Next() {
-		rows.Scan(&lang)
+		rows.Scan(&lang.Id, &lang.Abbreviation, &lang.Name)
+	}
+
+	// Check lang exist
+	if lang.Id == 0 {
+		return nil, nil
 	}
 
 	return &lang, nil
---
+++ api/internal/models/languages_test.go
@@ -0,0 +1,35 @@
+package models_test
+
+import (
+	"github.com/MoraGames/StreamingScheduler/core/internal/models"
+	"testing"
+)
+
+func TestLanguage_NewLanguage(t *testing.T) {
+
+	models.DbConn = initDB()
+
+	lang := &models.Language{
+		Abbreviation: "jpn",
+		Name:         "japanese",
+	}
+
+	lastId, err := lang.NewLanguage()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("ID:", lastId)
+}
+
+func TestGetLanguageById(t *testing.T) {
+
+	models.DbConn = initDB()
+
+	lang, err := models.GetLanguageById(5)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("Language:", lang)
+}
