924f3576feae3403957edda665181cb6362fadfb
KiritoNya <watashiwayuridaisuki@gmail.com>
2022-05-24 00:27:47 +0200

    Add Formats methods and tests

--- api/internal/models/formats.go
+++ api/internal/models/formats.go
@@ -1,6 +1,49 @@
 package models
 
-type Formats struct {
+type Format struct {
 	Id   int    `json:"id"`
 	Type string `json:"type"`
+}
+
+// NewFormat is a format method that adds a new format into database
+func (f *Format) NewFormat() (int64, error) {
+
+	qp, err := DbConn.Prepare(`INSERT INTO Formats(type) VALUES (?)`)
+	if err != nil {
+		return -1, err
+	}
+
+	res, err := qp.Exec(f.Type)
+	if err != nil {
+		return -1, err
+	}
+
+	return res.LastInsertId()
+}
+
+// GetFormatById is a function that gets the format from the database by id
+func GetFormatById(id int64) (*Format, error) {
+
+	var f Format
+
+	qp, err := DbConn.Prepare(`SELECT * FROM Formats WHERE id = ?`)
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := qp.Query(id)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		rows.Scan(&f.Id, &f.Type)
+	}
+
+	// Check lang exist
+	if f.Id == 0 {
+		return nil, nil
+	}
+
+	return &f, nil
 }
---
+++ api/internal/models/formats_test.go
@@ -0,0 +1,34 @@
+package models_test
+
+import (
+	"github.com/MoraGames/StreamingScheduler/core/internal/models"
+	"testing"
+)
+
+func TestFormat_NewFormat(t *testing.T) {
+
+	models.DbConn = initDB()
+
+	format := &models.Format{
+		Type: "1080p",
+	}
+
+	formatId, err := format.NewFormat()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("Format:", formatId)
+}
+
+func TestGetFormatById(t *testing.T) {
+
+	models.DbConn = initDB()
+
+	format, err := models.GetFormatById(1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("Format:", format)
+}
