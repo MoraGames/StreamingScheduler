cdf21fa7701610c821d139c110bb525ca99e23fb
KiritoNya <watashiwayuridaisuki@gmail.com>
2022-05-24 02:26:05 +0200

    Add db connect for tests

---
+++ api/internal/models/models_test.go
@@ -0,0 +1,44 @@
+package models_test
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+var DB_DRIVER = "mysql"
+var DB_USERNAME = "#####"
+var DB_PASSWORD = "######"
+var DB_HOSTNAME = "127.0.0.1"
+var DB_PORT = "3306"
+var DB_NAME = "#######"
+
+func initDB() *sql.DB {
+	var err error
+
+	// Create db url
+	dbUrl := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?parseTime=true&charset=utf8mb4",
+		DB_USERNAME,
+		DB_PASSWORD,
+		DB_HOSTNAME,
+		DB_PORT,
+		DB_NAME,
+	)
+
+	log.Printf(
+		"Connecting to db %s:%s/%s",
+		DB_HOSTNAME,
+		DB_PORT,
+		DB_NAME,
+	)
+
+	// Create the database handle, confirm driver is present
+	dbConn, err := sql.Open(DB_DRIVER, dbUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return dbConn
+}
