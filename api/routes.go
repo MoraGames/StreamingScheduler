e94262c28e5dacd6048501878294557f0271d712
KiritoNya <watashiwayuridaisuki@gmail.com>
2022-05-24 03:23:27 +0200

    Add routes

--- api/routes.go
+++ api/routes.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"github.com/MoraGames/StreamingScheduler/core/internal/controllers"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
-const ApiPrefix = "/api/v1"
+const ApiPrefix = "/v1"
 
 func NewRouter() *mux.Router {
 
@@ -13,11 +14,37 @@ func NewRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.Use(enableCors) //CORS middleware
 
-	// Define routes
-	//r.HandleFunc(ApiPrefix+"/login", login).Methods(http.MethodPost)
-	//r.HandleFunc(ApiPrefix+"/register", register).Methods(http.MethodPost)
-	//r.HandleFunc(ApiPrefix+"/refresh", refreshToken)
-	//r.HandleFunc(ApiPrefix+"/verify", verify)
+	// Episode routes
+	r.HandleFunc(ApiPrefix+"/episodes", controllers.CreateEpisode).Methods(http.MethodPost)
+	r.HandleFunc(ApiPrefix+"/episodes/{id}", controllers.GetEpisode).Methods(http.MethodGet)
+
+	// Event routes
+	r.HandleFunc(ApiPrefix+"/events", controllers.CreateEvent).Methods(http.MethodPost)
+	r.HandleFunc(ApiPrefix+"/events/{id}", controllers.GetEvent).Methods(http.MethodGet)
+
+	// Format routes
+	r.HandleFunc(ApiPrefix+"/formats", controllers.CreateFormat).Methods(http.MethodPost)
+	r.HandleFunc(ApiPrefix+"/formats/{id}", controllers.GetFormat).Methods(http.MethodGet)
+
+	// Language routes
+	r.HandleFunc(ApiPrefix+"/languages", controllers.CreateLanguage).Methods(http.MethodPost)
+	r.HandleFunc(ApiPrefix+"/languages/{id}", controllers.GetLanguage).Methods(http.MethodGet)
+
+	// Quality routes
+	r.HandleFunc(ApiPrefix+"/qualities", controllers.CreateQuality).Methods(http.MethodPost)
+	r.HandleFunc(ApiPrefix+"/qualities/{id}", controllers.GetQuality).Methods(http.MethodGet)
+
+	// Resource routes
+	r.HandleFunc(ApiPrefix+"/resources", controllers.CreateResource).Methods(http.MethodPost)
+	r.HandleFunc(ApiPrefix+"/resources/{id}", controllers.GetResource).Methods(http.MethodGet)
+
+	// Series routes
+	r.HandleFunc(ApiPrefix+"/series", controllers.CreateSeries).Methods(http.MethodPost)
+	r.HandleFunc(ApiPrefix+"/series/{id}", controllers.GetSeries).Methods(http.MethodGet)
+
+	// Titles routes
+	r.HandleFunc(ApiPrefix+"/titles", controllers.CreateTitle).Methods(http.MethodPost)
+	r.HandleFunc(ApiPrefix+"/titles/{id}", controllers.GetTitle).Methods(http.MethodGet)
 
 	return r
 }
